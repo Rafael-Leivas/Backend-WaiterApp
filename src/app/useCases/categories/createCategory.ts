8e0a65df74b7038b8ef061c5ba68b65720441390
NPU-Rafael-Leivas <rafael.leivas@npu.com.br>
2025-02-25 17:00:07 -0300

    Add error handling to createCategories function

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -3,9 +3,14 @@ import { Request, Response } from "express";
 import { Category } from "../../models/Category";
 
 export async function createCategories(req: Request, res: Response) {
-    const { name, icon } = req.body;
-    
-    const category = await Category.create({ name, icon });
+    try{
+        const { name, icon } = req.body;
+        
+        const category = await Category.create({ name, icon });
 
-    res.json(category);
+        res.status(201).json(category);
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 }
